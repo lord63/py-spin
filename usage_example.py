bf1546551307f2368fd68a9add0d2be0978b6f28
lord63 <lord63.j@gmail.com>
2015-05-03 10:01:55 +0800

    Add usage example

---
+++ usage_example.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals, print_function
+
+import time
+
+import spin
+from spin import make_spin
+
+
+@make_spin(spin.Spin1, "Downloading...")
+def demo():
+    print("Assume we're downloading a video")
+    print("It would cost much time.")
+    time.sleep(5)
+    print("Download success!")
+
+
+if __name__ == '__main__':
+    demo()
