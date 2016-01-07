ab5af95139f3e003ecc319cc63d816a806a14a48
lord63 <lord63.j@gmail.com>
2016-01-07 21:40:39 +0800

    Add python 3.5 in setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     keywords='terminal spin spinner',
     packages=['pyspin'],
